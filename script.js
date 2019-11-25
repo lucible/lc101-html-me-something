785ab8c54c6d3caca1e7c354355d045c852117c6
lucible <gobbldygook@letterboxes.org>
2019-11-24 18:09:55 -0600

    cleaned up the switch handler now there's only 1

--- script.js
+++ script.js
@@ -1,39 +1,32 @@
 function init () {
-    let wrapper = document.getElementById('wrapper')
+    // let wrapper = document.getElementById('wrapper')
+    // let lightSwitch = Array.from(document.getElementsByClassName('lightSwitch'))
+
     let curtain = document.getElementById('curtain')
-    let hiddenSwitch = document.getElementById('hiddenSwitch')
-    let lightSwitch = Array.from(document.getElementsByClassName('lightSwitch'))
+    let lightSwitch = document.getElementById('lightSwitch')
     let toggle = false
-    console.log(lightSwitch)
 
-    wrapper.addEventListener('click', (e) => {
-        if (e.target && e.target.matches('p.lightSwitch')) {
-            if (toggle === false) {
-                toggle = true
-                lightSwitch.map((item) => item.innerHTML = 'Turn the<br>lights on!')
-                curtain.style.visibility = "visible"
-                curtain.style.opacity = "100"
-                curtain.style.transition = "visibility 0s, opacity 0.5s ease"
-            } else {
-                toggle = false
-                lightSwitch.map((item) => item.innerHTML = 'Turn the<br>lights off...')
-                curtain.style.opacity = "0"
-                curtain.style.transition = "visibility 0.5s, opacity 0.5s ease"
-                curtain.style.visibility = "hidden"
-            }
-
+    lightSwitch.addEventListener('click', () => {
+        if (toggle === false) {
+            toggle = true
+            lightSwitch.innerHTML = 'Turn the<br>lights on!'
+            curtain.style.visibility = "visible"
+            curtain.style.opacity = "100"
+        } else {
+            toggle = false
+            lightSwitch.innerHTML = 'Turn the<br>lights off...'
+            curtain.style.opacity = "0"
+            curtain.style.transition = "visibility 0.5s, opacity 0.5s ease"
+            curtain.style.visibility = "hidden"
         }
     })
 
-    let img = document.getElementById('cover')
-    let imgPos = img.getBoundingClientRect()
-    let imgX = imgPos.left
-    let imgY = imgPos.top
+    let cover = document.getElementById('cover')
     let circle = document.getElementById('mask')
-    let circleRadius = circle.getAttribute('r') / 2
-    img.addEventListener('mousemove', (e) => {
-        circle.setAttribute('cx', e.clientX - imgX)
-        circle.setAttribute('cy', e.clientY - imgY)
+
+    cover.addEventListener('mousemove', (e) => {
+        circle.setAttribute('cx', e.clientX)
+        circle.setAttribute('cy', e.clientY)
     }, false)
 }
 

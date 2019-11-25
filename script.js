b827b30f43d51aebd9b2991b2471f65cdbb0f0d0
lucible <gobbldygook@letterboxes.org>
2019-11-24 20:23:12 -0600

    fixed overlay event propagation, plus mini styling

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 function init () {
-    // let wrapper = document.getElementById('wrapper')
-    // let lightSwitch = Array.from(document.getElementsByClassName('lightSwitch'))
+    let minis = Array.from(document.getElementsByClassName('minis'))
+
+    minis.map((item) => {
+        item.addEventListener('click', () => {
+            console.log(item.id)
+        }, true)
+    })
 
     let curtain = document.getElementById('curtain')
     let lightSwitch = document.getElementById('lightSwitch')
@@ -10,24 +15,37 @@ function init () {
         if (toggle === false) {
             toggle = true
             lightSwitch.innerHTML = 'Turn the<br>lights on!'
+
+            // black curtain fade in-out styling
             curtain.style.visibility = "visible"
             curtain.style.opacity = "100"
+
+            // hidden pictures fade in-out styling
+            minis.map((item) => item.style.transition = "visibility 0s, opacity 2.5s ease")
+            minis.map((item) => item.style.visibility = "visible")
+            minis.map((item) => item.style.opacity = "100")
         } else {
             toggle = false
             lightSwitch.innerHTML = 'Turn the<br>lights off...'
-            curtain.style.opacity = "0"
+
+            // black curtain fade in-out styling
             curtain.style.transition = "visibility 0.5s, opacity 0.5s ease"
             curtain.style.visibility = "hidden"
+            curtain.style.opacity = "0"
+
+            // hidden pictures fade in-out styling
+            minis.map((item) => item.style.transition = "visibility 0s, opacity 0s ease")
+            minis.map((item) => item.style.visibility = "hidden")
+            minis.map((item) => item.style.opacity = "0")
         }
     })
 
-    let cover = document.getElementById('cover')
     let circle = document.getElementById('mask')
 
-    cover.addEventListener('mousemove', (e) => {
+    wrapper.addEventListener('mousemove', (e) => {
         circle.setAttribute('cx', e.clientX)
         circle.setAttribute('cy', e.clientY)
-    }, false)
+    })
 }
 
 window.onload = init;
